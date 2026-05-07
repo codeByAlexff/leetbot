c18f40c2c1e7f856aba609fb1f2fd7bea03053a9
Alexander Ferreira <alexff@Alexanders-MacBook-Pro.local>
2026-05-06 20:56:05 -0400

    Fixed issues

--- leetcode.py
+++ leetcode.py
@@ -52,7 +52,7 @@ def leetUser(user):
          avatar = data["avatar"]
          ranking = data["ranking"]
          totalSolved = data2["totalSolved"]
-         totalSubmissions = data2["totalSubmissions"]
+         totalSubmissions = data2["totalSubmissions"]["submissions"]
          return username, avatar, ranking, totalSolved, totalSubmissions
     except (KeyError, TypeError):
          return None, None, None, None, None
